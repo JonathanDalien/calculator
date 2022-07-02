d34bcaef0070259171c3623e2fd4aa1fdf691bac
LegendeJJ <dalienjj@gmail.com>
2022-07-02 18:30:47 +0200

    Fixed bugs and added point button
    Fixed bugs that would appear if user tries to operate multiple numbers. Add warning that divide by 0 is not possible. Extended style.css, buttons now hav active effect.

--- script.js
+++ script.js
@@ -3,14 +3,21 @@ const screenlast = document.querySelector(".screen-last")
 const btnnums = document.querySelectorAll(".num")
 const btnoperators = document.querySelectorAll(".ops")
 const btnequals = document.querySelector(".equals")
+const btnclear = document.querySelector(".clear")
+const btndelete = document.querySelector(".delete")
+const btndot = document.querySelector(".dot")
 
 
 
 let resetscreen = false;
 screenCurrent.textContent = "0"
-let operator = "";
+screenlast.textContent = ""
+let operator = null;
 let firstoperand = "";
 let secoperand = "";
+
+
+btndot.addEventListener("click", adddot)
 
 btnnums.forEach((btn) => {
     btn.addEventListener("click", () => addNumber(btn.textContent))
@@ -22,7 +29,28 @@ btnoperators.forEach((btn) => {
 
 btnequals.addEventListener("click", () => evaluate())
 
+btnclear.addEventListener("click", clear)
+
+function clear() {
+    screenCurrent.textContent = "0";
+    screenlast.textContent = "";
+    firstoperand = "";
+    secoperand = "";
+    operator = null
+}
+
+function adddot() {
+    if (screenCurrent.textContent.includes(".")) { return }
+    screenCurrent.textContent += "."
+
+}
+
+function round(number) {
+    return Math.round(number * 1000) / 1000
+}
+
 function addoperator(op) {
+    if (operator !== null) evaluate()
     firstoperand = screenCurrent.textContent;
     operator = op;
     screenlast.textContent = `${firstoperand}${operator}`
@@ -32,13 +60,20 @@ function addoperator(op) {
 function addNumber(num) {
     if (screenCurrent.textContent === "0" || resetscreen) {
         screenCurrent.textContent = ""
+        resetscreen = false
     }
     screenCurrent.textContent += num;
 }
 
 function evaluate() {
+    if (operator === null || resetscreen) return
+    if (operator === "÷" && screenCurrent.textContent === "0") {
+        alert("Divide by 0 is not possible!")
+        return
+    }
     secoperand = screenCurrent.textContent
-    screenCurrent.textContent = eval(operator, firstoperand, secoperand)
+    screenCurrent.textContent = round(operate(operator, firstoperand, secoperand))
+    operator = null;
 }
 
 
@@ -58,7 +93,7 @@ function divide(a, b) {
     return a / b
 }
 
-function eval(operator, a, b) {
+function operate(operator, a, b) {
 
     a = Number(a);
     b = Number(b);
